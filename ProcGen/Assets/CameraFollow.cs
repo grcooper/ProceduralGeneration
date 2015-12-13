c1f51959dd9a1066b815ecb9135b8ff28ce3f46f
thadeaus <enfinityfilms@gmail.com>
2015-12-13 01:35:53 -0500

    Added movement that corresponds with direction. Next step is to add some ui to move from within the cardboard without using the keyboard

---
+++ ProcGen/Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        transform.position = player.transform.position;
+        Debug.Log(transform.position);
+    }
+
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+        transform.position = player.transform.position;
+        Debug.Log(transform.position);
+    }
+}
--- ProcGen/Assets/Player.cs
+++ ProcGen/Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 
     Rigidbody rigidbody;
     Vector3 velocity;
+    Quaternion rotation;
 
     void Start()
     {
@@ -15,10 +16,22 @@ public class Player : MonoBehaviour
     void Update()
     {
         velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * 10;
+        GameObject mainCamera = GameObject.Find("Head");
+        if (mainCamera)
+        {
+            rotation = mainCamera.transform.rotation;
+        }
+        else
+        {
+            rotation = transform.rotation;
+        }
     }
 
     void FixedUpdate()
     {
-        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+
+        rigidbody.rotation = rotation;
+        rigidbody.MovePosition(rigidbody.position + (transform.forward * velocity.z * Time.fixedDeltaTime));
+        rigidbody.MovePosition(rigidbody.position + (transform.right * velocity.x * Time.fixedDeltaTime));
     }
 }
